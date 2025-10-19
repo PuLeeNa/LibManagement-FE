c3bf56709012679071ac5723bb4f6064d39cc657
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-10-20 01:19:19 +0530

    BookConsole component to display a table of users instead of a placeholder message

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 // @ts-ignore: allow side-effect import of CSS without type declarations
 import './App.css';
 import NavB from './components/NavB';
+import { BookConsole } from './components/BookConsole';
 
 function App() {
   return (
     <>
     <NavB/>
+    <BookConsole/>
     </>
   );
 }
